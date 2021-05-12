ca23cfb70c0f1bc3775840b63d333d654a4ee977
Bartek <bartlomiejswies@gmail.com>
2021-05-12 20:37:45 +0200

    Sign up function bugfix

---
+++ public/js/signup.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#formSignup').validate({
+        rules: {
+            name: 'required',
+            email: {
+                required: true,
+                email: true,
+                remote: '/account/validate-email'
+            },
+            password: {
+                required: true,
+                minlength: 6,
+                validPassword: true
+            },
+            repeatedPassword: {
+                required: true
+            }
+        },
+        messages: {
+            name: 'Imie jest wymagane!',
+            email: {
+                required: 'E-mail jest wymagany!',
+                email: 'Adres e-mail nie jest poprawny!',
+                remote: 'E-mail jest już przypisany do innego konta!'
+            },
+            password: {
+                required: 'Hasło jest wymagane!',
+                minlength: 'Długość hasła musi wynosić conajmniej 6 znaków!',
+            },
+            repeatedPassword: {
+                required: 'Hasło jest wymagane!'
+            }
+        }
+    });
+})
